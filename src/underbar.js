055c4b95cf4e2b224e01256e3ead6dacb1e5fc3d
Sara Binns <sarabinns@gmail.com>
2013-10-18 04:16:28 -0400

    Added _.uniq

--- src/underbar.js
+++ src/underbar.js
@@ -103,6 +103,13 @@ var _ = { };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+    for (var i = 0; i<array.length; i++) {
+      if (unique.indexOf(array[i])=== -1) {
+        unique.push(array[i]);
+      }
+    }
+    return unique;
   };
 
 
