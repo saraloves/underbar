084134d0ab91fb298fe62c6f8d9f03f860ac7932
Sara Binns <sarabinns@gmail.com>
2013-10-18 05:45:33 -0400

    testing _.invoke

--- src/underbar.js
+++ src/underbar.js
@@ -145,6 +145,9 @@ var _ = { };
 
   // Calls the method named by methodName on each value in the list.
   _.invoke = function(list, methodName, args) {
+      for (var i = 0; i<list.length; i++) {
+        methodName(list[i]);
+      }
   };
 
   // Reduces an array or object to a single value by repetitively calling
