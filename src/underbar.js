9e41472e3b82fcac3003e1d078b852c7ef647ecb
Sara Binns <sarabinns@gmail.com>
2013-10-23 03:04:26 -0400

    test every

--- src/underbar.js
+++ src/underbar.js
@@ -203,25 +203,45 @@ _.contains = function(collection, target) {
   };
 
   // Determine whether all of the elements match a truth test.
-
-
-   _.every = function(collection, iterator) {
-
-    // Run the function every and take in the parameters collection 
-    //and iterator.  Collection is the array or object we are using,
-    //iterator is what we want to check for.  We know that the iterator
-    //is provided when we call _.every
-
+  
+  _.every = function(collection, iterator) {
     return _.reduce(collection, function(isTrue, item){
-    //Run the function reduce within every.  Reduce will iterate over
-    //a dataset and return a single value.  Here the inputs for reduce
-    //are collection, the iterator, and true. The inputs for the
-    //iterator are isTrue and item.  We should include iterator.
-     // console.log(isTrue, item, iterator);
-      return (isTrue === false ? false : iterator(item) === true); //this is running
+      //Run the function reduce within every.  Reduce will iterate over
+      //a dataset and return a single value.  Here the inputs for reduce
+      //are collection, the iterator, and true. The inputs for the
+      //iterator are isTrue and item.  We should include iterator.
+       // console.log(isTrue, item, iterator);
+    if (isTrue === false) {
+      return false;
+    } else {
+      if (iterator) {
+        return iterator(item) === true;
+      } else {
+        return item === true;
+      }
+    }
     }, true);
-
   };
+
+  //  _.every = function(collection, iterator) {
+
+  //   // Run the function every and take in the parameters collection 
+  //   //and iterator.  Collection is the array or object we are using,
+  //   //iterator is what we want to check for.  We know that the iterator
+  //   //is provided when we call _.every
+
+  // //   return _.reduce(collection, function(isTrue, item){
+  // //   //Run the function reduce within every.  Reduce will iterate over
+  // //   //a dataset and return a single value.  Here the inputs for reduce
+  // //   //are collection, the iterator, and true. The inputs for the
+  // //   //iterator are isTrue and item.  We should include iterator.
+  // //    // console.log(isTrue, item, iterator);
+  // //     return (isTrue === false ? false : iterator(item) === true); //this is running
+  // //   }, true);
+
+  // // };
+
+  // end of previously working every
 
   // _.every = function(collection, iterator) {
   //   // TIP: Try re-using reduce() here.
